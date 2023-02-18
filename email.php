e9f194afebe692a0f05413198e04ffc157015028
Otto386 <120267695+Otto386@users.noreply.github.com>
2023-02-18 21:10:27 +0100

    Update email.php

--- email.php
+++ email.php
@@ -5,7 +5,7 @@ require 'email.php'; // Підключаємо бібліотеку SendGrid
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY')); // Створюємо екземпляр класу SendGrid з використанням API-ключа
 $email = new \SendGrid\Mail\Mail(); // Створюємо об'єкт листа
 
-$email->setFrom("example@example.com", "Example User"); // Встановлюємо адресу відправника
+$email->setFrom("worksearch8888@seznam.cz", "Example User"); // Встановлюємо адресу відправника
 $email->setSubject("Тестовий лист"); // Встановлюємо тему листа
 $email->addTo("worksearch8888@seznam.cz", "Recipient Name"); // Встановлюємо адресу отримувача
 $email->addContent("text/plain", "Привіт!"); // Встановлюємо текст повідомлення
